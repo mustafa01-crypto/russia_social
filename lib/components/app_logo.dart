a3992ae9d8fb6d5412de83db9ab0a40b1f4b3f7a
mustafa01-crypto <hasansabbah284@gmail.com>
2021-09-25 17:22:07 +0300

    final

--- lib/components/app_logo.dart
+++ lib/components/app_logo.dart
@@ -3,7 +3,7 @@ import 'package:sizer/sizer.dart';
 
 Widget appLogo() {
   return SizedBox(
-      width: 45.13.w,
+      width: 50.w,
       height: 7.1.h,
       child: Image.asset(
         "assets/logo.png",
